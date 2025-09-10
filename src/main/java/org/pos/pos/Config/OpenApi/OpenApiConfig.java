71d6bb1f9cec8c484fad4a2d8970047658e4d81b
nrv01 <developer.alexander@outlook.com>
2025-09-09 22:20:44 -0500

    fix: Added security item and API info in OpenApi configuration

--- src/main/java/org/pos/pos/Config/OpenApi/OpenApiConfig.java
+++ src/main/java/org/pos/pos/Config/OpenApi/OpenApiConfig.java
@@ -2,6 +2,8 @@ package org.pos.pos.Config.OpenApi;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,14 +12,21 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI api(){
-        var scheme = new SecurityScheme();
-        scheme.name("bearerAuth")
+    public OpenAPI api() {
+        final String schemeName = "bearerAuth";
+
+        SecurityScheme scheme = new SecurityScheme()
+                .name(schemeName)
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
                 .bearerFormat("JWT");
 
         return new OpenAPI()
-                .components(new Components().addSecuritySchemes("bearerAuth", scheme));
+                .info(new Info()
+                        .title("POS API")
+                        .description("REST API for POS system with JWT authentication")
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+                .components(new Components().addSecuritySchemes(schemeName, scheme));
     }
  }
